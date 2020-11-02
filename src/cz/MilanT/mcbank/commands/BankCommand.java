bb91603932d1fc4621b7ebc602bf488617b74400
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-02 14:27:32 +0100

    Deleted useless imports

--- src/cz/MilanT/mcbank/commands/BankCommand.java
+++ src/cz/MilanT/mcbank/commands/BankCommand.java
@@ -11,7 +11,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class BankCommand implements CommandExecutor {
     private final ConfigManager configManager;
