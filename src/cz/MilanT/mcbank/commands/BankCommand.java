567e5201445c34502a3efcecfe0257ac3fe83886
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-02 13:06:21 +0100

    Added some structure to onCommand method in BankCommand.java and added default help command and bank account status command.

--- src/cz/MilanT/mcbank/commands/BankCommand.java
+++ src/cz/MilanT/mcbank/commands/BankCommand.java
@@ -2,9 +2,11 @@ package cz.MilanT.mcbank.commands;
 
 import cz.MilanT.mcbank.libraries.Vault;
 import cz.MilanT.mcbank.managers.ConfigManager;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class BankCommand implements CommandExecutor {
     private final ConfigManager configManager;
@@ -17,6 +19,25 @@ public class BankCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(sender instanceof Player) {
+            Player player = (Player)sender;
+
+            if(args.length == 0) {
+                player.sendMessage("/bank");
+                player.sendMessage("/bank status");
+                player.sendMessage("/bank pay <player> <amount>");
+            }
+
+            if(args.length > 1) {
+                Economy economy = vault.getEcon();
+                if(args[0].equalsIgnoreCase("status")) {
+                    player.sendMessage("§e" + player.getName() + "§7, your current account balance is §a" + economy.getBalance(player));
+                }
+            }
+        } else {
+            sender.sendMessage("§cThis command is enabled only for players!");
+        }
+
         return true;
     }
 
