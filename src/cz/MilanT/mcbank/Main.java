cec169e44796a16886adb9a89675930d5d048409
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-02 12:32:12 +0100

    Deleted useless imports from Main.java

--- src/cz/MilanT/mcbank/Main.java
+++ src/cz/MilanT/mcbank/Main.java
@@ -5,11 +5,7 @@ import cz.MilanT.mcbank.libraries.Vault;
 import cz.MilanT.mcbank.listeners.PlayerListener;
 import cz.MilanT.mcbank.managers.ConfigManager;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
