e646f060dba86eecca99fa82d2992aab7b64005f
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-07 16:59:07 +0100

    Added IStorage as constructor property in EconomyAPI and used this interface in class methods.

--- src/cz/MilanT/mcbank/vault/EconomyAPI.java
+++ src/cz/MilanT/mcbank/vault/EconomyAPI.java
@@ -1,21 +1,23 @@
 package cz.MilanT.mcbank.vault;
 
-import cz.MilanT.mcbank.system.player.AccountManager;
+import cz.MilanT.mcbank.storage.IStorage;
+import cz.MilanT.mcbank.system.player.Account;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.OfflinePlayer;
 
 import net.milkbowl.vault.economy.EconomyResponse.ResponseType;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EconomyAPI implements Economy {
-    private final AccountManager accountManager;
     private final String name;
+    private final IStorage storage;
 
-    public EconomyAPI(AccountManager accountManager) {
-        this.accountManager = accountManager;
+    public EconomyAPI(IStorage storage) {
+        this.storage = storage;
         this.name = "McBank Economy";
     }
 
@@ -36,32 +38,32 @@ public class EconomyAPI implements Economy {
 
     @Override
     public int fractionalDigits() {
-        return 0;
+        return -1;
     }
 
     @Override
     public String format(double amount) {
-        return null;
+        return String.valueOf(amount);
     }
 
     @Override
     public String currencyNamePlural() {
-        return null;
+        return "";
     }
 
     @Override
     public String currencyNameSingular() {
-        return null;
+        return "";
     }
 
     @Override
     public boolean hasAccount(String playerName) {
-        return false;
+        return storage.hasPlayerAccount(playerName);
     }
 
     @Override
     public boolean hasAccount(OfflinePlayer player) {
-        return false;
+        return storage.hasPlayerAccount(player.getName());
     }
 
     @Override
@@ -76,12 +78,12 @@ public class EconomyAPI implements Economy {
 
     @Override
     public double getBalance(String playerName) {
-        return 0;
+        return storage.getPlayerAccount(playerName).getBalance();
     }
 
     @Override
     public double getBalance(OfflinePlayer player) {
-        return 0;
+        return this.getBalance(player.getName());
     }
 
     @Override
@@ -96,12 +98,12 @@ public class EconomyAPI implements Economy {
 
     @Override
     public boolean has(String playerName, double amount) {
-        return false;
+        return storage.getPlayerAccount(playerName).getBalance() >= amount;
     }
 
     @Override
     public boolean has(OfflinePlayer player, double amount) {
-        return false;
+        return this.has(player.getName(), amount);
     }
 
     @Override
@@ -116,105 +118,120 @@ public class EconomyAPI implements Economy {
 
     @Override
     public EconomyResponse withdrawPlayer(String playerName, double amount) {
-        return null;
+        if(!hasAccount(playerName)) return new EconomyResponse(0,0, ResponseType.FAILURE, "Player don't have a account");
+        if(amount < 0) return new EconomyResponse(0,0, ResponseType.FAILURE, "Cannot deposit negative amount number");
+
+        Account account = this.storage.getPlayerAccount(playerName);
+        double result = account.getBalance() - amount;
+        if(result < 0) return new EconomyResponse(0,0, ResponseType.FAILURE, "Player don't have a balance for this withdraw");
+        this.storage.setPlayerBalance(playerName, result);
+
+        return new EconomyResponse(amount, result, ResponseType.SUCCESS, null);
     }
 
     @Override
     public EconomyResponse withdrawPlayer(OfflinePlayer player, double amount) {
-        return null;
+        return this.withdrawPlayer(player.getName(), amount);
     }
 
     @Override
     public EconomyResponse withdrawPlayer(String playerName, String worldName, double amount) {
-        return null;
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse withdrawPlayer(OfflinePlayer player, String worldName, double amount) {
-        return null;
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse depositPlayer(String playerName, double amount) {
-        return null;
+        if(!hasAccount(playerName)) return new EconomyResponse(0,0, ResponseType.FAILURE, "Player don't have a account");
+        if(amount < 0) return new EconomyResponse(0,0, ResponseType.FAILURE, "Cannot deposit negative amount number");
+
+        Account account = this.storage.getPlayerAccount(playerName);
+        double result = amount + account.getBalance();
+        this.storage.setPlayerBalance(playerName, result);
+
+        return new EconomyResponse(amount, result, ResponseType.SUCCESS, null);
     }
 
     @Override
     public EconomyResponse depositPlayer(OfflinePlayer player, double amount) {
-        return null;
+        return this.depositPlayer(player.getName(), amount);
     }
 
     @Override
     public EconomyResponse depositPlayer(String playerName, String worldName, double amount) {
-        return null;
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse depositPlayer(OfflinePlayer player, String worldName, double amount) {
-        return null;
+        return this.notImplemented();
     }
 
     /**
      * Function for creating EconomyResponse which is returned if another plugin call Banks.
      * @return EconomyResponse
      */
-    private EconomyResponse banksNotSupported() {
-        return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED, "Plugin McBank not support this banks functions.");
+    private EconomyResponse notImplemented() {
+        return new EconomyResponse(0, 0, ResponseType.NOT_IMPLEMENTED, "Plugin McBank not support this function");
     }
 
     @Override
     public EconomyResponse createBank(String name, String player) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse createBank(String name, OfflinePlayer player) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse deleteBank(String name) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse bankBalance(String name) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse bankHas(String name, double amount) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse bankWithdraw(String name, double amount) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse bankDeposit(String name, double amount) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse isBankOwner(String name, String playerName) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse isBankOwner(String name, OfflinePlayer player) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse isBankMember(String name, String playerName) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
     public EconomyResponse isBankMember(String name, OfflinePlayer player) {
-        return this.banksNotSupported();
+        return this.notImplemented();
     }
 
     @Override
@@ -224,12 +241,19 @@ public class EconomyAPI implements Economy {
 
     @Override
     public boolean createPlayerAccount(String playerName) {
+        Account account = new Account(playerName, 0);
+        try {
+            return storage.createPlayerAccount(account);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
     public boolean createPlayerAccount(OfflinePlayer player) {
-        return false;
+        return this.createPlayerAccount(player.getName());
     }
 
     @Override
