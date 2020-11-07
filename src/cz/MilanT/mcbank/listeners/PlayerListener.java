bfc87479eef5f2ffae8414615a526165502108a0
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-07 11:58:22 +0100

    Deleted Vault instances from PlayerListener and added EconomyHandler parameter to constructor

--- src/cz/MilanT/mcbank/listeners/PlayerListener.java
+++ src/cz/MilanT/mcbank/listeners/PlayerListener.java
@@ -1,8 +1,8 @@
 package cz.MilanT.mcbank.listeners;
 
 import cz.MilanT.mcbank.constants.Variables;
-import cz.MilanT.mcbank.libraries.Vault;
 import cz.MilanT.mcbank.managers.ConfigManager;
+import cz.MilanT.mcbank.vault.EconomyHandler;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -13,17 +13,17 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
     private final ConfigManager configManager;
-    private final Vault vault;
+    private final EconomyHandler economyHandler;
 
-    public PlayerListener(ConfigManager configManager, Vault vault) {
+    public PlayerListener(ConfigManager configManager, EconomyHandler economyHandler) {
         this.configManager = configManager;
-        this.vault = vault;
+        this.economyHandler = economyHandler;
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        Economy economy = vault.getEcon();
+        Economy economy = economyHandler.getEcon();
         FileConfiguration configuration = this.configManager.getConfig();
 
         double actualDeposit = configuration.getDouble("events.joinEvent.deposit");
@@ -42,15 +42,15 @@ public class PlayerListener implements Listener {
 
     public void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        Economy economy = vault.getEcon();
+        Economy economy = economyHandler.getEcon();
         FileConfiguration configuration = this.configManager.getConfig();
 
         economy.depositPlayer(player, configuration.getDouble("events.quitEvent.deposit"));
         economy.withdrawPlayer(player, configuration.getDouble("events.quitEvent.withdraw"));
     }
 
-    public Vault getVault() {
-        return vault;
+    public EconomyHandler getEconomyHandler() {
+        return economyHandler;
     }
 
     public ConfigManager getConfigManager() {
