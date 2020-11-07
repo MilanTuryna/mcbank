ae50c8efb06cb37dc9a63f4cc1a677191b990fbc
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-07 12:01:31 +0100

    Deleted Vault instances from Main plugin class and added create of database connection in ::onEnable method.

--- src/cz/MilanT/mcbank/Main.java
+++ src/cz/MilanT/mcbank/Main.java
@@ -1,35 +1,55 @@
 package cz.MilanT.mcbank;
 
+import cz.MilanT.mcbank.db.Database;
+import cz.MilanT.mcbank.managers.ConfigManager;
+
 import cz.MilanT.mcbank.commands.AdminBankCommand;
 import cz.MilanT.mcbank.commands.BankCommand;
-import cz.MilanT.mcbank.libraries.Vault;
+
+import cz.MilanT.mcbank.system.player.AccountManager;
+import cz.MilanT.mcbank.vault.EconomyAPI;
+import cz.MilanT.mcbank.vault.EconomyHandler;
 import cz.MilanT.mcbank.listeners.PlayerListener;
-import cz.MilanT.mcbank.managers.ConfigManager;
+
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.SQLException;
 
 public class Main extends JavaPlugin implements Listener {
     public boolean vaultActive = false; // variable for checking if vault working
 
     @Override
     public void onEnable() {
-        Vault vault = new Vault(this);
         ConfigManager configManager = new ConfigManager(this);
-        this.saveDefaultConfig();
-        this.getConfig().options().copyDefaults(true);
 
-        if(vault.setupEconomy()) {
-            this.log("Plugin was enabled and VaultAPI is perfectly working.");
-            this.vaultActive = true;
+        try {
+            Database database = configManager.getDatabase();
 
-            this.getCommand("mcbank").setExecutor(new BankCommand(this, configManager, vault));
-            this.getCommand("adminbank").setExecutor(new AdminBankCommand(configManager, vault));
+            AccountManager accountManager = new AccountManager(database);
+            EconomyAPI economyAPI = new EconomyAPI(accountManager);
+            EconomyHandler economyHandler = new EconomyHandler(this, economyAPI);
 
-            this.getServer().getPluginManager().registerEvents(new PlayerListener(configManager, vault), this);
-        } else {
-            this.log("§cVault not found, plugin will be disabled.");
-            this.log("§aFor good work is needed: §ehttps://github.com/MilkBowl/Vault");
+            this.saveDefaultConfig();
+            this.getConfig().options().copyDefaults(true);
+
+            if(economyHandler.setupEconomy()) {
+                this.log("Plugin was enabled and VaultAPI is perfectly working.");
+                this.vaultActive = true;
+
+                this.getCommand("mcbank").setExecutor(new BankCommand(this, configManager, economyHandler));
+                this.getCommand("adminbank").setExecutor(new AdminBankCommand(configManager, economyHandler));
+
+                this.getServer().getPluginManager().registerEvents(new PlayerListener(configManager, economyHandler), this);
+            } else {
+                this.log("§cVault not found, plugin will be disabled.");
+                this.log("§aFor good work is needed: §ehttps://github.com/MilkBowl/Vault");
+                this.getPluginLoader().disablePlugin(this);
+            }
+        } catch(SQLException sqlException) {
+            sqlException.printStackTrace();
+            this.log("§cAn error occurred while connecting to the database, McBank plugin will be disabled. &a&l>> Check logs for solution.");
             this.getPluginLoader().disablePlugin(this);
         }
     }
