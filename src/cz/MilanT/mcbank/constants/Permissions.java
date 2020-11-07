6baf0e66075960092d6a8765dec1be5e7fd1e57b
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-07 11:56:58 +0100

    Added ADMIN permission node constant to Permissions

--- src/cz/MilanT/mcbank/constants/Permissions.java
+++ src/cz/MilanT/mcbank/constants/Permissions.java
@@ -3,6 +3,8 @@ package cz.MilanT.mcbank.constants;
 public final class Permissions {
     private Permissions() { }
 
+    public static final String ADMIN = "mcbank.admin";
+
     public static final String COMMAND_HELP = "mcbank.help";
     public static final String COMMAND_STATUS = "mcbank.status";
     public static final String COMMAND_PAY = "mcbank.pay";
