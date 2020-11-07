541da9569785f6df32f8cf136b4d952b4fe13bae
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-07 12:59:00 +0100

    Added Storage\IStorage interface and added Constants\Storage class.

---
+++ src/cz/MilanT/mcbank/constants/Storage.java
@@ -0,0 +1,10 @@
+package cz.MilanT.mcbank.constants;
+
+public class Storage {
+    private Storage() {
+
+    }
+
+    private static final String MYSQL = "mysql";
+    private static final String YAML = "yaml";
+}
---
+++ src/cz/MilanT/mcbank/storage/IStorage.java
@@ -0,0 +1,5 @@
+package cz.MilanT.mcbank.storage;
+
+public interface IStorage {
+
+}
