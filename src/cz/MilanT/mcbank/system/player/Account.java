1b7fe1bb8c7fbb9dea74f06ae70d0999549adfea
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-07 12:37:33 +0100

    Added Player player, AccountManager accountManager constructor properties to Account class and added getter methods for this properties.

--- src/cz/MilanT/mcbank/system/player/Account.java
+++ src/cz/MilanT/mcbank/system/player/Account.java
@@ -1,4 +1,24 @@
 package cz.MilanT.mcbank.system.player;
 
+import org.bukkit.entity.Player;
+
+/**
+ * Class for better manipulation with Accounts from AccountManager
+ */
 public class Account {
+    private final Player player;
+    private final AccountManager accountManager;
+
+    public Account(Player player, AccountManager accountManager) {
+        this.player = player;
+        this.accountManager = accountManager;
+    }
+
+    public AccountManager getAccountManager() {
+        return accountManager;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
 }
