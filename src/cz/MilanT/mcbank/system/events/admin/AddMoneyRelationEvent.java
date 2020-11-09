294614d0b0a28b580294054180ebba13b9989833
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-09 09:41:25 +0100

    Added class annotations to children of AdminMoneyRelationEvent class

--- src/cz/MilanT/mcbank/system/events/admin/AddMoneyRelationEvent.java
+++ src/cz/MilanT/mcbank/system/events/admin/AddMoneyRelationEvent.java
@@ -1,5 +1,8 @@
 package cz.MilanT.mcbank.system.events.admin;
 
+/**
+ * Event for handling action after Administrator add money to player bank account
+ */
 public class AddMoneyRelationEvent extends AdminMoneyRelationEvent {
     public AddMoneyRelationEvent(String administratorName, String targetName, double amount) {
         super(administratorName, targetName, amount);
--- src/cz/MilanT/mcbank/system/events/admin/RemoveMoneyRelationEvent.java
+++ src/cz/MilanT/mcbank/system/events/admin/RemoveMoneyRelationEvent.java
@@ -1,5 +1,8 @@
 package cz.MilanT.mcbank.system.events.admin;
 
+/**
+ * Event for handling action after Administrator remove money from Player bank account
+ */
 public class RemoveMoneyRelationEvent extends AdminMoneyRelationEvent {
     public RemoveMoneyRelationEvent(String administratorName, String targetName, double amount) {
         super(administratorName, targetName, amount);
