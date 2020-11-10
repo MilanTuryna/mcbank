d0f4927e97e12e5f821a6ef069ead9a984cd47ab
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-10 14:50:07 +0100

    Created MoneyBag class with static constants for getting keys from configuration.

---
+++ src/cz/MilanT/mcbank/constants/MoneyBag.java
@@ -0,0 +1,11 @@
+package cz.MilanT.mcbank.constants;
+
+public class MoneyBag {
+    private MoneyBag(){}
+
+    public static final String NBT_TAG = "mcbank_money";
+
+    public static final String LORE = "moneybag.lore";
+    public static final String ITEM_URL = "moneybag.item_url";
+    public static final String NAME = "moneybag.display_name";
+}
