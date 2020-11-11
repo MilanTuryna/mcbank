06f5fae112855a8503682e77dffa0a159e8d8077
MilanTuryna <miloslavturyna@seznam.cz>
2020-11-11 17:04:20 +0100

    Changed constats values in MoneyBag

--- src/cz/MilanT/mcbank/constants/MoneyBag.java
+++ src/cz/MilanT/mcbank/constants/MoneyBag.java
@@ -5,7 +5,7 @@ public class MoneyBag {
 
     public static final String NBT_TAG = "mcbank_money";
 
-    public static final String LORE = "moneybag.lore";
-    public static final String ITEM_URL = "moneybag.item_url";
-    public static final String NAME = "moneybag.display_name";
+    public static final String LORE = "moneyBag.lore";
+    public static final String ITEM_URL = "moneyBag.item_url";
+    public static final String NAME = "moneyBag.display_name";
 }
